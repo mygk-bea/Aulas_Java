a2e2687b1921b6593a179be5301521153bfceacc
mygk-bea <meyaguskobeatriz@gmail.com>
2024-10-04 15:14:54 -0300

    organizando visualização - Aula 9

--- projeto-aula/src/pages/Home/index.jsx
+++ projeto-aula/src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import './Home.css';
-import Card from '../../components/Card';
+import Card from '../../components/General/Card';
 
 function Home() {
   return (
@@ -7,9 +7,9 @@ function Home() {
       <div className='container'>
         <h1>Projetos</h1>
         <div className="content">
-          <Card nome="Ganhe um Conselho!" aula="Aula 7" data="09/09/2024" rota="/conselhos"/>
-          <Card nome="Listas - Exercício" aula="Aula 8" data="16/09/2024" rota="/listas-exercicio"/>
           <Card nome="Inputs - Exercício" aula="Aula 9" data="23/09/2024" rota="/inputs-exercicio"/>
+          <Card nome="Listas - Exercício" aula="Aula 8" data="16/09/2024" rota="/listas-exercicio"/>
+          <Card nome="Ganhe um Conselho!" aula="Aula 7" data="09/09/2024" rota="/conselhos"/>
         </div>
       </div>
     </main>
--- projeto-aula/src/pages/Inputs/index.jsx
+++ projeto-aula/src/pages/Inputs/index.jsx
@@ -4,6 +4,10 @@ import FeedbackForm from "../../components/Inputs/FeedbackForm";
 import Form from "../../components/Inputs/Form";
 import Gallery from "../../components/Inputs/Gallery";
 import LightSwitch from "../../components/Inputs/LightSwitch";
+import { NavLink } from 'react-router-dom';
+import { IconBackPage } from '../../assets/icons/IconBackPage';
+import '../Lists/Lists.css';
+import './Inputs.css';
 
 function ExInputs() {
     function handleColorChange() {
@@ -12,12 +16,44 @@ function ExInputs() {
 
     return (
         <>
-            <LightSwitch />
-            <ColorSwitch onChangeColor={handleColorChange} />
-            <Gallery />
-            <Form />
-            <FeedbackForm />
-            <FeedbackButton />
+            <header className='header'>
+                <div className="logo">
+                    Exercícios Inputs
+                </div>
+                <nav>
+                    <NavLink to="/">
+                        Voltar Para Projetos
+                        <IconBackPage/>
+                    </NavLink>
+                </nav>
+            </header>
+
+            <div className='main main_inputs'>
+                <div id="exercicio1">
+                    <h3>Ex 1.</h3>
+                    <LightSwitch />
+                </div>
+                <div id="exercicio2">
+                    <h3>Ex 2.</h3>
+                    <ColorSwitch onChangeColor={handleColorChange} />
+                </div>
+                <div id="exercicio3">
+                    <h3>Ex 3.</h3>
+                    <Gallery />
+                </div>
+                <div id="exercicio4">
+                    <h3>Ex 4.</h3>
+                    <Form />
+                </div>
+                <div id="exercicio5">
+                    <h3>Ex 5.</h3>
+                    <FeedbackForm />
+                </div>
+                <div id="exercicio6">
+                    <h3>Ex 6.</h3>
+                    <FeedbackButton />
+                </div>
+            </div>
         </>
     );
 }
